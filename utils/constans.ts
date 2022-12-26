3e4b145fc806e6365616639389de6d96e6a04f4e
tuanpa21 <phamtuan210394@gmail.com>
2022-12-26 16:26:56 +0700

    Add mock server with data

---
+++ utils/constans.ts
@@ -0,0 +1,9 @@
+export const mockInventories = [
+  { name: 'Refrigerator' },
+  { name: 'Television' },
+  { name: 'Sofa' },
+  { name: 'Light Bulb' },
+  { name: 'Table' },
+  { name: 'Chair' },
+  { name: 'Bed' },
+]
