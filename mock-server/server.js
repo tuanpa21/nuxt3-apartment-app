3b606d422d7818c442693f38a744d9123070cd3a
tuanpa21 <phamtuan210394@gmail.com>
2022-12-27 23:16:10 +0700

    fix: fix vercel deployment

--- mock-server/server.js
+++ mock-server/server.js
@@ -5,14 +5,16 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
   })
 )
 server.use(router)
-server.listen(3000, '0.0.0.0', () => {
+server.listen(3001, () => {
   console.log('JSON Server is running')
 })
 
+// Export the Server API
 module.exports = server
