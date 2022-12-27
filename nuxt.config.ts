32326d14b705c9551c72e2ada1b6f3c02ccd1e44
tuanpa21 <phamtuan210394@gmail.com>
2022-12-27 10:30:38 +0700

    Change app name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Apartment Management App',
+      meta: [{ name: 'description', content: 'Apartment Management App' }],
+    },
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
